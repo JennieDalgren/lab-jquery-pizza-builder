33e157d3af861c01960e06093575e20473aad431
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-08-23 16:40:36 +0200

    made some functions

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -4,9 +4,40 @@
 $( document ).ready(function() {
 
   var totalPrice = 13;
+  var newValue;
   $("strong").html("$ " + totalPrice);
-  console.log(totalPrice);
 
+  function findOutTypeOfTopping(topping) {
+    switch(topping) {
+      case 'btn-pepperonni'    :
+      case 'btn-green-peppers' :
+      case 'btn-mushrooms'     :
+        newValue = 1;
+        updatePrice(topping);
+        break;
+      case 'btn-crust' :
+        newValue = 5;
+        updatePrice(topping);
+        break;
+      case 'btn-sauce' :
+        newValue = 3;
+        updatePrice(topping);
+        break;
+    }
+  }
+
+  function updatePrice (toppingClassName) {
+
+    if ($('.' + toppingClassName).hasClass("active")){
+      totalPrice += newValue;
+      $("strong").html("$ " + totalPrice);
+    }
+    else {
+      totalPrice -= newValue;
+      $("strong").html("$ " + totalPrice);
+    }
+
+  }
 
   $(".crust").toggleClass("crust-gluten-free");
   $(".btn-crust").toggleClass("active");
@@ -19,86 +50,37 @@ $( document ).ready(function() {
     $(".pep").toggle();
     $(this).toggleClass("active");
     $("#price-list li:first").toggle();
-    if ($(".btn-pepperonni").hasClass("active")){
-      console.log("I have active class");
-      totalPrice += 1;
-      console.log(totalPrice);
-      $("strong").html("$ " + totalPrice);
-
-    }
-    else {
-      totalPrice -= 1;
-      console.log(totalPrice);
-      $("strong").html("$ " + totalPrice);
-    }
-
+    findOutTypeOfTopping(this.classList[1]);
   });
 
   $(".btn-green-peppers").on("click", function() {
     $(".green-pepper").toggle();
     $(this).toggleClass("active");
     $("#price-list li:nth(1)").toggle();
-    if ($(".btn-green-peppers").hasClass("active")){
-      console.log("I have active class");
-      totalPrice += 1;
-      $("strong").html("$ " + totalPrice);
-    }
-    else {
-      totalPrice -= 1;
-      $("strong").html("$ " + totalPrice);
-    }
+    findOutTypeOfTopping(this.classList[1]);
   });
 
   $(".btn-mushrooms").on("click", function() {
     $(".mushroom").toggle();
     $(this).toggleClass("active");
     $("#price-list li:nth(2)").toggle();
-    if ($(".btn-mushrooms").hasClass("active")){
-      console.log("I have active class");
-      totalPrice += 1;
-      $("strong").html("$ " + totalPrice);
-    }
-    else {
-      totalPrice -= 1;
-      $("strong").html("$ " + totalPrice);
-    }
-
+    findOutTypeOfTopping(this.classList[1]);
   });
 
   $(".btn-crust").on("click", function() {
     $(".crust").toggleClass("crust-gluten-free");
     $(this).toggleClass("active");
     $("#price-list li:last").toggle();
-    if ($(".btn-crust").hasClass("active")){
-      console.log("I have active class");
-      totalPrice += 5;
-      $("strong").html("$ " + totalPrice);
-    }
-    else {
-      totalPrice -= 5;
-      $("strong").html("$ " + totalPrice);
-    }
-
+    findOutTypeOfTopping(this.classList[1]);
   });
 
   $(".btn-sauce").on("click", function() {
     $(".sauce").toggleClass("sauce-white");
     $(this).toggleClass("active");
     $("#price-list li:nth(3)").toggle();
+    findOutTypeOfTopping(this.classList[1]);
     // checkPrice(this);
-    if ($(".btn-sauce").hasClass("active")){
-      console.log("I have active class");
-      totalPrice += 3;
-      $("strong").html("$ " + totalPrice);
-    }
-    else {
-      totalPrice -= 3;
-      $("strong").html("$ " + totalPrice);
-    }
-
   });
-
-
 
 //   function checkPrice ($element) {
 //
